bc6749a5024a8cae1e8e604be3bbeab62d3a1218
Sophie Ostmeier <99991488+SophieOstmeier@users.noreply.github.com>
2022-09-30 13:50:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,23 @@ from setuptools import setup
 setup(
     name='UncertainSmallEmpty',
     version='0.1',
-    packages=[''],
-    url='',
-    license='',
+    packages=['UncertainSmallEmpty'],
+    url='https://github.com/SophieOstmeier/UncertainSmallEmpty.git',
+    license='Apache License Version 2.0, January 2004',
     author='SophieOstmeier',
     author_email='sostm@stanford.edu',
-    description=''
+    description='Evaluation for Medical Image Segemantion Models for Uncertain, Small and Empty Reference Annotations'
+    install_requires=[
+        "numpy"
+        "MedPy"
+        "scipy"
+        "pandas"
+        "SimpleITK"
+        "batchgenerators"
+        "nibabel"
+        "natsort"
+        "argparse"
+        "ctg-surface-distance"
+        "openpyxl"
+    ]
 )
