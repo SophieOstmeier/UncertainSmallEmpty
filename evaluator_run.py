3e709cc39e651c9f302f8fb594d179a2e68e5c2e
Sophie Ostmeier <99991488+SophieOstmeier@users.noreply.github.com>
2022-11-11 08:02:04 -0800

    Update evaluator_run.py

--- evaluator_run.py
+++ evaluator_run.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
                         help="Path to folder with the segmentation you would like to compare to the ground truth segmentations. "
                              "Must be in in .nii.gz format."
                              "The filename has to be the same as the filename of the corresponding gt.")
-    parser.add_argument("-number_classes", required=False, default=1,type=int,
+    parser.add_argument("-number_classes", required=False, default=2,type=int,
                         help="number of segmentation classes including background class, for example '2' for binary"
                              "segmentation task")
     parser.add_argument("-threshold", required=False, default=False, type=int,
